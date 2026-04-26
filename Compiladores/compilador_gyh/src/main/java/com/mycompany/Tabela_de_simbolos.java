307bd185177c0adf0ec5e9e71799aeb6b1ab0a75
guigiumon <guigiumon@gmail.com>
2026-04-26 17:26:28 -0300

    Criação da tabela de simbolos, próximo passo: adaptar analisador lexico a colocar variáveis na tabela de simbolos

---
+++ Compiladores/compilador_gyh/src/main/java/com/mycompany/Tabela_de_simbolos.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany;
+import java.util.HashMap;
+
+/**
+ *
+ * @author Adm
+ */
+public class Tabela_de_simbolos {
+    HashMap<String, Object> tabela;
+    public Tabela_de_simbolos() {
+        this.tabela = new HashMap<>();
+    }
+}
